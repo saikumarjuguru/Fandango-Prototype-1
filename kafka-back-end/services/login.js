38adb09d7a1e1033f38f5dd2686b2e3a73fcaa55
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-04-11 13:53:43 -0700

    login bug

--- kafka-back-end/services/login.js
+++ kafka-back-end/services/login.js
@@ -1,4 +1,4 @@
-var mongoURL = "mongodb://localhost:27017/fandango";
+//var mongoURL = "mongodb://localhost:27017/fandango";
 var ObjectID = require('mongodb').ObjectID;
 var mongo = require("./mongo");
 var auth = require('passport-local-authenticate');
@@ -25,26 +25,33 @@ function handle_request(msg,callback){
     console.log('in login',msg);
     User.findOne({useroremail: msg.user.username}, function(err, user) {
         if(err) throw err;
-        if(!user) {
+        console.log(user);
+        if(!user || user!=null) {
             res.code =  401;
             data = {message : "Failed Login"};
             res.value = data;
+            callback(null,res);
           }
+          else{
           bcrypt.compare(msg.user.password, user.password, function(err, isMatch){
             if(err) throw err;
             if(isMatch) { 
                 res.code=200;
                 data = {username: user.useroremail};
+                callback(null,res);
                
             }
             else {
                 res.code =  401;
                 data = {message : "Failed Login"};
                 res.value = data;
+                callback(null,res);
             }
             
+            
           });
-          callback(null,res);
+        }
+         
         
     });
 }
--- server/routes/kafka/client.js
+++ server/routes/kafka/client.js
@@ -1,13 +1,33 @@
+// var rpc = new (require('./kafkarpc'))();
+// var kafka = require('kafka-node');
+// var KeyedMessage = kafka.KeyedMessage;
+
+// //make request to kafka
+// function make_request(queue_name, key, msg_payload, callback){
+//     console.log('in make request');
+//     console.log(msg_payload.username);
+//     var keyedMessage = new KeyedMessage(key, msg_payload);
+// 	  rpc.makeRequest(queue_name, keyedMessage, function(err, response){
+
+// 		if(err)
+// 			console.error(err);
+// 		else{
+// 			console.log("response", response);
+// 			callback(null, response);
+// 		}
+// 	});
+// }
+
+// exports.make_request = make_request;
+
+
 var rpc = new (require('./kafkarpc'))();
-var kafka = require('kafka-node');
-var KeyedMessage = kafka.KeyedMessage;
 
 //make request to kafka
-function make_request(queue_name, key, msg_payload, callback){
+function make_request(queue_name, msg_payload, callback){
     console.log('in make request');
-    console.log(msg_payload.username);
-    var keyedMessage = new KeyedMessage(key, msg_payload);
-	  rpc.makeRequest(queue_name, keyedMessage, function(err, response){
+    console.log(msg_payload);
+	rpc.makeRequest(queue_name, msg_payload, function(err, response){
 
 		if(err)
 			console.error(err);
