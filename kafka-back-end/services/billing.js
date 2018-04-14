22ae99da81232fbd776020572db4e9b23f52b94b
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-04-13 18:49:14 -0700

    get bill details

--- kafka-back-end/services/billing.js
+++ kafka-back-end/services/billing.js
@@ -9,6 +9,7 @@ function handle_request(msg, callback){
     };
 
     console.log(msg);
+    if(msg.type=='add_bill'){
     var bill = msg.bill;
     var result = new Billing(bill);
             result.save(function (err) {
@@ -18,6 +19,17 @@ function handle_request(msg, callback){
             res.success = true;
             res.message = result;
     callback(null, res);
+    }
+    if(msg.type=='get_bill'){
+        Billing.findById(msg.bill_id, function(err, bill){
+            if(err) throw err;
+            if(bill){
+                res.success =  true;
+                res.message = bill;
+                callback(null,res);
+            }
+        });
+    }
 }
 
 exports.handle_request = handle_request;
