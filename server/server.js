6c1460dfb6b2ac0011e5385870b2c39b8db0b6a9
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-04-24 18:17:15 -0700

    fddf

--- server/server.js
+++ server/server.js
@@ -67,10 +67,10 @@ app.use('/signup',signup);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
-  //  res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', true);
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
