fbbf7dbfe5358ce4d58717b5f81a2682f69d887e
zenobia panvelwalla <zenobiapanvelwalla@gmail.com>
2018-04-24 18:17:30 -0700

    dfdf

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import NewerHomePage from "./components/NewerHomePage";
 import {BrowserRouter} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory'
+import { createLocationSubscriber } from 'react-redux-analytics'
 
 class App extends Component {
 
