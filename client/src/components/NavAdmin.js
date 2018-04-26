391fc88f9b0487edb708e5e672a588b21de061fc
AishwaryaSaxena <aishwarya.saxena@sjsu.edu>
2018-04-26 16:51:28 -0700

    **Admin Search start**

--- client/src/components/NavAdmin.js
+++ client/src/components/NavAdmin.js
@@ -5,21 +5,37 @@ class NavAdmin extends Component {
 
   constructor(props){
     super(props);
-   
+    this.searchMovies = this.searchMovies.bind(this);
   } 
+
+  searchMovies(userInput){
+    
+    console.log(userInput.serachText.value);
+    console.log(userInput.searchType.value);
+    localStorage.setItem('searchTextAdmin',userInput.serachText.value);
+    localStorage.setItem('searchTypeAdmin',userInput.searchType.value);
+
+    switch(userInput.searchType.value)
+    {
+        case "Movie":this.props.history.push('/adminmoviesearch');break;
+        case "Movie Hall":this.props.history.push('/adminhallsearch');break;
+        case "Bill By Date":
+        case "Bill By Month":this.props.history.push('/adminbillsearch');break;
+    }
+   
+  }
   
   render(){
     console.log("render");
     return(
         <nav class="navbar navbar-expand-lg navbar-white bg-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
         
+        <a class="navbar-brand">
+        <img src='images/fandango-header.png' width='210px'/>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+
           <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item"> 
-            <a class="nav-link text-warning" href="#">Fandango</a></li>
             <li class="nav-item active">
               <a class="nav-link text-warning" href="./adminhome">Home <span class="sr-only">(current)</span></a>
             </li>
@@ -36,15 +52,15 @@ class NavAdmin extends Component {
           <ul class="nav navbar-nav navbar-right">
                 <form class="form-inline my-2 my-lg-0">
                     <div class="form-group">
-                        <select class="form-control" ref="rating" id="exampleFormControlSelect1">
+                        <select class="form-control" ref="searchType" id="exampleFormControlSelect1">
                             <option>Movie</option> 
                             <option>Movie Hall</option>  
                             <option>Bill By Date</option>  
                             <option>Bill By Month</option>  
                         </select>
-                    </div>&nbsp;
-                    <input class="form-control" ref="serachText" type="search" placeholder="Search Movies..." aria-label="Search"/>
-                    <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" onClick={()=>this.searchMovies(this.refs.serachText.value)}>Search</button>
+                    </div>
+                    <input class="form-control my-2 my-lg-0" ref="serachText" type="search" placeholder="Search Here..." aria-label="Search"/>
+                    <button class="btn btn-outline-warning my-2 my-sm-0" type="submit" onClick={()=>this.searchMovies(this.refs)}>Search</button>
                 </form>&nbsp;
                 <a href='./adminhalladd'><button class="btn btn btn-warning navbar-btn">Add Movie Hall</button> </a>&nbsp;
                 <a href='./adminaddmovie'><button class="btn btn btn-warning navbar-btn">Add Movie</button></a>
