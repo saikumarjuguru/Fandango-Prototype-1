03b3b624c6ac519941d785ad79dd8f516f69ff53
Tarun Arora <tarunarora0807@gmail.com>
2018-04-29 16:07:19 -0700

    link fix

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -240,7 +240,7 @@ class LandingPage extends Component {
                     return (
                       <div className="col-lg-4 col-md-6 mb-4" key={index}>
                         <div className="card h-100">
-                          <a href="#"><img className="card-img-top" src={movie.photos} /></a>
+                          <Link to={movieDetailURL}><img className="card-img-top" src={movie.photos} /></Link>
                           <div className="card-body">
                             <h5 className="card-title">
                               <Link to={movieDetailURL}>{movie.movie_name}</Link>                            
